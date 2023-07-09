1315a4acd5f61aadf94d07914a049796528946b2
Khushiii789 <139000518+Khushiii789@users.noreply.github.com>
2023-07-09 17:27:12 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+img_left=Image.open(r"C:\Users\User\Pictures\Screenshots\student.png")
+        img_left=img_left.resize((720,130),Image.ANTIALIAS)
+        self.photoimg_left=ImageTk.PhotoImage(img_left)
+
+        f_lbl=Label(Left_frame,image=self.photoimg_left)
+        f_lbl.place(x=5,y=0,width=720,height=130)
